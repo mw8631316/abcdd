6c9d6fcadf546c94d29f7fa174fefd4603889fa7
mw8631316 <mw8631316@gmail.com>
2020-01-05 14:30:00 +0500

    updatae

---
+++ hi/src/main.rs
@@ -0,0 +1,7 @@
+//this is 1st project
+fn main() {
+    println!("Hello, world!");
+    println!("wellcome to rust");
+    let age = 22 ;
+    println!("the value of age is : {}",age);
+}
---
+++ inam/src/main.rs
@@ -0,0 +1,12 @@
+//#[drive(debug)]
+//enum option<T> {
+  //  some(T),
+    //none,
+//}
+
+fn main() {
+    let value1 = Some(5);
+    println!("{:?}",value1);
+    let coa = string::from ("Qamar Javeed Bajwa"),;
+    println!("name leanth of {:?} is {}",coa,coa.len.());
+}
